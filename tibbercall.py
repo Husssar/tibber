3b6aed84c22b26305b6a3401e2c24cc3d365a6df
Tobias Svensson <husssar@gmail.com>
2023-01-05 11:19:56 +0100

    Adding mainfile
    Adding mainfile. need to build dockerfile, but will
    do in another commit

---
+++ tibbercall.py
@@ -0,0 +1,60 @@
+import requests
+import cred
+import pymysql
+
+URL = 'https://api.tibber.com/v1-beta/gql'
+KEY = cred.key
+USER = cred.user
+PASSWORD = cred.password
+HOST = cred.host
+DATABASE = cred.database
+
+def insert_data(value):
+    cnx = None
+    qry = []
+    try:
+        cnx = pymysql.connect(user=USER, password=PASSWORD, host=HOST, database=DATABASE)
+        cnx.autocommit = True
+        for r in value:
+
+            qry = (f"INSERT IGNORE INTO grid_cost (when_price, price, tax, totalprice) "
+                   f"VALUES (\"{r['startsAt']}\", {r['energy']}, {r['tax']}, {r['total']})")
+            print(qry)
+            cur = cnx.cursor()
+            cur.execute(qry)
+            cnx.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cnx.close()
+
+
+# KEY registerd at tibber.se
+def get_data(when):
+    headers = {
+        'Authorization': 'Bearer '+ KEY +'',
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'query':
+            '{viewer '
+                 '{homes '
+                 '{currentSubscription '
+                     '{priceInfo '
+                         '{' + when + ' '
+                            '{total energy tax startsAt }'
+                            '}'
+                        '}'
+                    '}'
+                 '}'
+            '}',
+    }
+    response = requests.post('https://api.tibber.com/v1-beta/gql', headers=headers, json=json_data)
+
+    #print(response.json())
+    data = response.json()
+   # print(data)
+    price = data['data']['viewer']['homes'][0]['currentSubscription']['priceInfo'][when]
+    return price
+
